1151d2c28677928bed9abed343e3d00547cbac16
zac.ramsey <zac.ramsey@teamsparq.com>
2023-09-27 13:01:31 -0600

    Basic Setup for h2 in memory db

---
+++ h2-demo/src/main/java/com/teamsparq/h2demo/H2DemoApplication.java
@@ -0,0 +1,15 @@
+package com.teamsparq.h2demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ConfigurableApplicationContext;
+
+@SpringBootApplication
+public class H2DemoApplication {
+
+	public static void main(String[] args) {
+SpringApplication.run(H2DemoApplication.class, args);
+	}
+
+
+}
