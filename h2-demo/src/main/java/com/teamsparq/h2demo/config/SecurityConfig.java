1f4e9e9a51dfb1c9f52840bec4f3be962a0b1427
zac.ramsey <zac.ramsey@teamsparq.com>
2023-09-28 12:33:51 -0600

    Setting up authentication for In-memory-database

--- h2-demo/src/main/java/com/teamsparq/h2demo/config/SecurityConfig.java
+++ h2-demo/src/main/java/com/teamsparq/h2demo/config/SecurityConfig.java
@@ -3,22 +3,81 @@ package com.teamsparq.h2demo.config;
 import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabase;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseBuilder;
+import org.springframework.jdbc.datasource.embedded.EmbeddedDatabaseType;
 import org.springframework.security.config.Customizer;
+import org.springframework.security.config.annotation.method.configuration.EnableMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configurers.HeadersConfigurer;
+import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.jdbc.JdbcDaoImpl;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.provisioning.InMemoryUserDetailsManager;
+import org.springframework.security.provisioning.JdbcUserDetailsManager;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.servlet.util.matcher.MvcRequestMatcher;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.handler.HandlerMappingIntrospector;
 
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
+
 @Configuration
 @EnableWebSecurity
 @EnableWebMvc
+@EnableMethodSecurity
 public class SecurityConfig {
 
     // My enpdoints start from /v1 so this pattern is ok for me
     private static final String API_URL_PATTERN = "/v1/**";
+
+
+    //    @Bean
+//    public InMemoryUserDetailsManager users() {
+//        return new InMemoryUserDetailsManager(
+//                User.withUsername("zac")
+//                        .password("{noop}password")
+//                        .roles("ADMIN")
+////                        .authorities("read")
+//                        .build()
+//        );
+//    }
+
+    @Bean
+    DataSource dataSource() {
+        return new EmbeddedDatabaseBuilder()
+                .setType(EmbeddedDatabaseType.H2)
+                .setName("bookstore")
+                .addScript(JdbcDaoImpl.DEFAULT_USER_SCHEMA_DDL_LOCATION)
+                .build();
+    }
+
+    @Bean
+    JdbcUserDetailsManager users(DataSource dataSource, PasswordEncoder encoder) {
+       UserDetails admin = User.builder()
+                .username("admin")
+                .password(encoder.encode("my_secret_password_1234"))
+                .roles("ADMIN")
+                .build();
+
+       UserDetails user = User.builder()
+                .username("Zac")
+                .password(encoder.encode("password"))
+                .roles("ADMIN")
+                .build();
+
+
+        JdbcUserDetailsManager jdbcUserDetailsManager = new JdbcUserDetailsManager(dataSource);
+        jdbcUserDetailsManager.createUser(admin);
+        jdbcUserDetailsManager.createUser(user);
+        return jdbcUserDetailsManager;
+    }
+
 
     @Bean
     public SecurityFilterChain getSecurityFilterChain(HttpSecurity http,
@@ -33,18 +92,23 @@ public class SecurityConfig {
                 headersConfigurer.frameOptions(HeadersConfigurer.FrameOptionsConfig::sameOrigin));
 
         http.authorizeHttpRequests(auth ->
-                auth
-                        .requestMatchers(mvcMatcherBuilder.pattern(API_URL_PATTERN)).permitAll()
-                        //This line is optional in .authenticated() case as .anyRequest().authenticated()
-                        //would be applied for H2 path anyway
-                        .requestMatchers(PathRequest.toH2Console()).permitAll()
-                        .anyRequest().authenticated()
-        );
-
-        http.formLogin(Customizer.withDefaults());
-        http.httpBasic(Customizer.withDefaults());
+                        auth
+                                .requestMatchers(mvcMatcherBuilder.pattern(API_URL_PATTERN)).permitAll()
+                                //This line is optional in .authenticated() case as .anyRequest().authenticated()
+                                //would be applied for H2 path anyway
+                                .requestMatchers(PathRequest.toH2Console()).permitAll()
+                                .anyRequest().authenticated()
+                )
+//                .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
+                .formLogin(Customizer.withDefaults())
+                .httpBasic(Customizer.withDefaults());
 
         return http.build();
+    }
+
+    @Bean
+    PasswordEncoder passwordEncoder() {
+        return new BCryptPasswordEncoder();
     }
 }
 
