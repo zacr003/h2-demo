e74b97abb401ccbce9a45bc942ab7c22b0d18314
zac.ramsey <zac.ramsey@teamsparq.com>
2023-10-03 14:20:38 -0600

    Modification to Book entity adds hibernate validation property to application.properties

--- h2-demo/src/main/java/com/teamsparq/h2demo/model/Book.java
+++ h2-demo/src/main/java/com/teamsparq/h2demo/model/Book.java
@@ -5,9 +5,11 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
 
 
 @Entity
+@Table
 public class Book {
     @Id
     @GeneratedValue(strategy = GenerationType.AUTO)
