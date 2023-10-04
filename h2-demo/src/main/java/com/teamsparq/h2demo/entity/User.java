c680ea38c9b95761f6ad6b151b8a08579847a7de
zac.ramsey <zac.ramsey@teamsparq.com>
2023-10-04 11:39:50 -0600

    Implements UserDetailsImpl and UserDetailsServiceImpl, modifies Role and User entities

--- h2-demo/src/main/java/com/teamsparq/h2demo/entity/User.java
+++ h2-demo/src/main/java/com/teamsparq/h2demo/entity/User.java
@@ -16,6 +16,7 @@ import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 
 import java.util.HashSet;
+import java.util.Optional;
 import java.util.Set;
 
 @Entity
@@ -24,6 +25,8 @@ import java.util.Set;
         @UniqueConstraint(columnNames = "email")
 })
 public class User {
+
+
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Integer id;
@@ -53,5 +56,45 @@ public class User {
         this.username = username;
         this.email = email;
         this.password = password;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public Set<Role> getRoles() {
+        return roles;
+    }
+
+    public void setRoles(Set<Role> roles) {
+        this.roles = roles;
     }
 }
--- h2-demo/src/main/java/com/teamsparq/h2demo/service/UserDetailsServiceImpl.java
+++ h2-demo/src/main/java/com/teamsparq/h2demo/service/UserDetailsServiceImpl.java
@@ -1,4 +1,26 @@
 package com.teamsparq.h2demo.service;
 
-public class UserDetailsServiceImpl {
+import com.teamsparq.h2demo.entity.User;
+import com.teamsparq.h2demo.repository.UserRepository;
+import jakarta.transaction.Transactional;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class UserDetailsServiceImpl implements UserDetailsService {
+
+    @Autowired
+    UserRepository repository;
+
+    @Override
+    @Transactional
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        User user = repository.findByUsername(username)
+                .orElseThrow(() -> new UsernameNotFoundException("User Not Found with username: " + username));
+
+        return UserDetailsImpl.build(user);
+    }
 }
